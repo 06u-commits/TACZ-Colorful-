81379bd4a64a24199b53d572246896c596fe7abd
06u-commits <06user1990@gmail.com>
2026-09-23 14:27:43 +0300

    Add TritiumConfig class with color and edit properties

--- src/main/java/com/tacz/guns/client/resource/pojo/display/TritiumConfig.java
+++ src/main/java/com/tacz/guns/client/resource/pojo/display/TritiumConfig.java
@@ -1,1 +1,33 @@
+package com.tacz.guns.client.resource.pojo.display;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.awt.*;
+
+public class TririumConfig {
+    private Integer defaultColor;
+
+    @Expose
+    @SerializedName("default_color")
+    private String color = "#FF0000";
+
+    @Expose
+    @SerializedName("can_edit")
+    private boolean canEdit = true;
+
+    public int getDefaultColor() {
+        if (defaultColor == null) {
+            try {
+                defaultColor = Color.decode(color).getRGB();
+            } catch (NumberFormatException e) {
+                defaultColor = Color.WHITE.getRGB();
+            }
+        }
+        return defaultColor;
+    }
+
+    public boolean canEdit() {
+        return canEdit;
+    }
+}
