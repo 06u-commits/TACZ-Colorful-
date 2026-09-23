562ee9e767b5996deb8caaa9b6d32cd9b7bdf775
06u-commits <06user1990@gmail.com>
2026-09-23 14:35:44 +0300

    Rename LaserColorUtil to TritiumColorUtil and update methods

--- src/main/java/com/tacz/guns/util/TritiumColorUtil.java
+++ src/main/java/com/tacz/guns/util/TritiumColorUtil.java
@@ -9,15 +9,15 @@ import com.tacz.guns.client.resource.pojo.display.LaserConfig;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
-public class LaserColorUtil {
-    public static int getLaserColor(ItemStack stack, @NotNull LaserConfig defaultConfig) {
+public class TritiumColorUtil {
+    public static int getTritiumColor(ItemStack stack, @NotNull TritiumConfig defaultConfig) {
         if (stack == null) {
             return defaultConfig.getDefaultColor();
         }
 
         if (stack.getItem() instanceof IAttachment iAttachment) {
-            if (iAttachment.hasCustomLaserColor(stack)) {
-                return iAttachment.getLaserColor(stack);
+            if (iAttachment.hasCustomTritiumColor(stack)) {
+                return iAttachment.getTritiumColor(stack);
             } else {
                 return defaultConfig.getDefaultColor();
             }
@@ -34,7 +34,7 @@ public class LaserColorUtil {
         return defaultConfig.getDefaultColor();
     }
 
-    public static int getLaserColor(ItemStack stack) {
+    public static int getTritiumColor(ItemStack stack) {
         if (stack == null) {
             return 0xFF0000;
         }
@@ -45,18 +45,18 @@ public class LaserColorUtil {
             } else {
                 return TimelessAPI.getClientAttachmentIndex(iAttachment.getAttachmentId(stack))
                         .map(ClientAttachmentIndex::getLaserConfig)
-                        .map(LaserConfig::getDefaultColor)
+                        .map(TritiumConfig::getDefaultColor)
                         .orElse(0xFF0000);
             }
         }
 
         if (stack.getItem() instanceof IGun gun) {
-            if (gun.hasCustomLaserColor(stack)) {
-                return gun.getLaserColor(stack);
+            if (gun.hasCustomTritiumColor(stack)) {
+                return gun.getTritiumColor(stack);
             } else {
                 return TimelessAPI.getGunDisplay(stack)
                         .map(GunDisplayInstance::getLaserConfig)
-                        .map(LaserConfig::getDefaultColor)
+                        .map(TritiumConfig::getDefaultColor)
                         .orElse(0xFF0000);
             }
         }
